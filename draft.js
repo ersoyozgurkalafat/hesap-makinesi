8422f2638ad03e233602417e465583f0a21cd799
Celal Durmuşoğlu <durmusoglucelal@outlook.com>
2025-07-04 10:37:54 +0300

    done

--- draft.js
+++
@@ -1,9 +0,0 @@
-const clear = (param) => {
-  const currentIndex = operation.length - 1;
-  if(operation[currentIndex].length > 1){
-    operation[currentIndex].pop();
-  }
-  else if(operation[currentIndex].length == 1){
-    operation.pop();
-  }
-};
